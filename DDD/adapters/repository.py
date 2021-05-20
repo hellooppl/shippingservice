78befe3ce9e1da4bbaefb3115c8956f496b6e69d
hellooppl <palpaliadarsha@gmail.com>
2021-05-20 15:35:48 +0545

    added repository for delivery agent

--- DDD/adapters/repository.py
+++ DDD/adapters/repository.py
@@ -108,51 +108,51 @@ class ShippingRepository(AbstractRepository):
                 self[i]["is_deleted"] = False
             return "{user} is deleted successfully"
 
-# class Deliveryrepository:
-#     def get(self, user: UUID) -> Delivery:
-#         delivery = {}
-#         if user in delivery_list[user]:
-#             batch = delivery_list[user]
-#         return Delivery.construct(delivery)
-#
-#     def add(self, model: Delivery):
-#         values = {
-#             "user": model.user,
-#             "name": model.name,
-#             "post": model.post,
-#             "permission": model.permission,
-#             "available": model.available,
-#             "task": model.task,
-#         }
-#         model.append(values)
-#
-#     def update(self, model: Delivery):
-#         values = {
-#             "user": model.user,
-#             "name": model.name,
-#             "post": model.post,
-#             "permission": model.permission,
-#             "available": model.available,
-#             "task": model.task,
-#         }
-#
-#         for i in range(len(self) + 1):
-#             if self[i]["user"] == values.user:
-#                 self[i].update(values)
-#
-#     def delete(self, model: Shipping):
-#         values = {
-#             "id_": model.id_,
-#             "category": model.category,
-#             "cost": model.cost,
-#             "regionId": model.regionId,
-#             "orderId": model.orderId,
-#             "insurance": model.insurance,
-#             "date_to_ship": model.date_to_ship,
-#         }
-#
-#         for i in range(len(self) + 1):
-#             if self[i]["user"] == values.user:
-#                 self[i]["is_deleted"] == False
-#             return "{user} is deleted successfully"
-#
+class Deliveryrepository:
+    def get(self, user: UUID) -> Delivery:
+        delivery = {}
+        if user in delivery_list[user]:
+            batch = delivery_list[user]
+        return Delivery.construct(delivery)
+
+    def add(self, model: Delivery):
+        values = {
+            "user": model.user,
+            "name": model.name,
+            "post": model.post,
+            "permission": model.permission,
+            "available": model.available,
+            "task": model.task,
+        }
+        model.append(values)
+
+    def update(self, model: Delivery):
+        values = {
+            "user": model.user,
+            "name": model.name,
+            "post": model.post,
+            "permission": model.permission,
+            "available": model.available,
+            "task": model.task,
+        }
+
+        for i in range(len(self) + 1):
+            if self[i]["user"] == values.user:
+                self[i].update(values)
+
+    def delete(self, model: Shipping):
+        values = {
+            "id_": model.id_,
+            "category": model.category,
+            "cost": model.cost,
+            "regionId": model.regionId,
+            "orderId": model.orderId,
+            "insurance": model.insurance,
+            "date_to_ship": model.date_to_ship,
+        }
+
+        for i in range(len(self) + 1):
+            if self[i]["user"] == values.user:
+                self[i]["is_deleted"] == False
+            return "{user} is deleted successfully"
+
